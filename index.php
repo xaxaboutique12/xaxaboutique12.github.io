06b2d687c3cc83bfe14aca9ea08a3be6948d8a35
xaxaboutique12 <43950769+xaxaboutique12@users.noreply.github.com>
2018-10-08 16:35:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ switch (ENVIRONMENT)
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-	$system_path = 'lib';
+	$system_path = 'index.ph';
 
 /*
  *---------------------------------------------------------------
